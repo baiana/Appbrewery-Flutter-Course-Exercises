b4326dfdb7c5ff9057e47c445ce9ceefbec380ee
ana Dias <analuisa104@gmail.com>
2020-04-11 17:48:54 -0300

    feat: add network image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello Word"),
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[200],
+      appBar: AppBar(
+        title: Text("Eu sou Rica"),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              "https://i.pinimg.com/originals/53/a3/58/53a358b2538861e52f68a82a2378b190.gif"),
+        ),
+      ),
     ),
   ));
 }
