45101b63b533dd96b1daeff329fee50ad2553724
ana Dias <analuisa104@gmail.com>
2020-04-17 14:19:23 -0300

    feat: create home widget

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
+        theme: ThemeData.dark(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pergunte-me Tudo"),
+        elevation: 6,
+      ),
+      body: Container(
+        color: Colors.green.shade700,
+      ),
+    );
+  }
+}
