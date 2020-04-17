1e98c3522b82962749c87e04c8e3689c1be5c4ad
ana Dias <analuisa104@gmail.com>
2020-04-17 14:20:13 -0300

    fix: change scaffold background to green

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -11,12 +11,13 @@ class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green.shade700,
       appBar: AppBar(
         title: Text("Pergunte-me Tudo"),
         elevation: 6,
       ),
       body: Container(
-        color: Colors.green.shade700,
+        color: ,
       ),
     );
   }
