46fcdb3a817a04369a660dc10ca2ba63b4f0376b
ana Dias <analuisa104@gmail.com>
2020-04-18 14:33:56 -0300

    feat: distribute keys on screen

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
@@ -8,15 +9,41 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: FlatButton(
-            onPressed: () {
-              final player = AudioCache();
-              player.play('note1.wav');
-            },
-            child: Text("Clica"),
-          ),
+          child: XylophonePage(),
         ),
+      ),
+    );
+  }
+}
+
+class XylophonePage extends StatelessWidget {
+  final player = AudioCache();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        displayKey(1, Colors.red, player),
+        displayKey(2, Colors.orangeAccent, player),
+        displayKey(3, Colors.yellow, player),
+        displayKey(4, Colors.green, player),
+        displayKey(5, Colors.green.shade900, player),
+        displayKey(6, Colors.lightBlue, player),
+        displayKey(7, Colors.purpleAccent, player),
+      ],
+    );
+  }
+
+  Expanded displayKey(int keyNumber, Color color, AudioCache player) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          player.play('note$keyNumber.wav');
+        },
       ),
     );
   }
