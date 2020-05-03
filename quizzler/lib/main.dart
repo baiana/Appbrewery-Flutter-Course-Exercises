ea70215bde1610ec92538adecae2f442fb399ab5
ana Dias <analuisa104@gmail.com>
2020-05-03 13:01:03 -0300

    feat: create list of questions

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -25,6 +25,23 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  static Icon right = Icon(
+    Icons.check,
+    color: Colors.lightGreenAccent,
+  );
+  static Icon wrong = Icon(
+    Icons.close,
+    color: Colors.redAccent,
+  );
+
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +54,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +78,10 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(() {
+                  scoreKeeper.add(right);
+                });
+
                 //The user picked true.
               },
             ),
@@ -79,12 +100,24 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(() {
+                  scoreKeeper.add(wrong);
+                });
                 //The user picked false.
+              },
+              onLongPress: () {
+                setState(() {
+                  scoreKeeper.clear();
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Expanded(
+          child: Row(
+            children: scoreKeeper,
+          ),
+        )
       ],
     );
   }
